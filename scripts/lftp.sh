91a99f2026d02f7a5a61e85b8e8d2c09dd259118
ashtonian <github@ashtonkinslow.com>
2019-04-08 00:13:07 -0500

    lftp

---
+++ scripts/lftp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+login=""
+pass=""
+host=""
+remote_dir='~/files/completed/'
+local_dir="/mnt/temp/lftp/"
+
+base_name="$(basename "$0")"
+lock_file="/tmp/$base_name.lock"
+trap "rm -f $lock_file; exit 0" SIGINT SIGTERM
+if [[ -e "$lock_file" ]]
+then
+    echo "$base_name is running already."
+    exit
+else
+    touch "$lock_file"
+    lftp -p 22 -u "$login","$pass" sftp://"$host" << EOF
+    set sftp:auto-confirm yes
+    set mirror:use-pget-n 5
+    set xfer:use-temp-file yes
+    set xfer:temp-file-name *.lftp
+    mirror --Remove-source-dirs --loop --no-perms -c -P5 --log="/mnt/temp/$base_name.log" "$remote_dir" "$local_dir"
+    quit
+EOF
+    rm -f "$lock_file"
+    trap - SIGINT SIGTERM
+    exit
+fi
