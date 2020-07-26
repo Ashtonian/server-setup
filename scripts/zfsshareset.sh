222feea68e524611be38225df9a80c0346a46e58
Ashton Kinslow <github@ashtonkinslow.com>
2020-07-26 04:09:36 -0500

    Update zfsshareset.sh

--- scripts/zfsshareset.sh
+++ scripts/zfsshareset.sh
@@ -22,3 +22,38 @@ lutece/backup
 
 sudo zfs set \share="name=docs,path=/lutece/docs,prot=nfs,sec=sys,rw=10.0.0.40" \
 lutece/docs
+
+
+
+sudo zfs set sharenfs="sec=sys,rw=10.0.0.40" \
+lutece/apps
+
+sudo zfs set sharenfs="sec=sys,rw=10.0.0.40" \
+lutece/media
+
+sudo zfs set sharenfs="sec=sys,rw=10.0.0.40" \
+lutece/public
+
+sudo zfs set sharenfs="sec=sys,rw=10.0.0.40" \
+lutece/scripts
+
+sudo zfs set sharenfs="sec=sys,rw=10.0.0.40" \
+lutece/fam
+
+sudo zfs set sharenfs="sec=sys,rw=10.0.0.40" \
+lutece/drop
+
+sudo zfs set sharenfs="sec=sys,rw=10.0.0.40" \
+lutece/backup
+
+sudo zfs set sharenfs="sec=sys,rw=10.0.0.40" \
+lutece/docs
+
+sudo zfs set sharenfs=on lutece/apps
+sudo zfs set sharenfs=on lutece/media
+sudo zfs set sharenfs=on lutece/public
+sudo zfs set sharenfs=on lutece/scripts
+sudo zfs set sharenfs=on lutece/fam
+sudo zfs set sharenfs=on lutece/drop
+sudo zfs set sharenfs=on lutece/backup
+sudo zfs set sharenfs=on lutece/docs
