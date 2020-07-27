8e0ecbdbfffcd71ba57da80ca12d854df55e4ea7
Ashton Kinslow <github@ashtonkinslow.com>
2020-07-27 02:22:46 -0500

    Create mount_nfs.sh

---
+++ scripts/mount_nfs.sh
@@ -0,0 +1,8 @@
+mount -t nfs 10.0.0.40:/apps /mnt/apps
+mount -t nfs 10.0.0.40:/media /mnt/media
+mount -t nfs 10.0.0.40:/public /mnt/public
+mount -t nfs 10.0.0.40:/scripts /mnt/scripts
+mount -t nfs 10.0.0.40:/fam /mnt/fam
+mount -t nfs 10.0.0.40:/drop /mnt/drop
+mount -t nfs 10.0.0.40:/backup /mnt/backup
+mount -t nfs 10.0.0.40:/docs /mnt/docs
